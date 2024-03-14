5a53cab365df08d2318d8894f4a55a29110ccc9d
Daniel Nikitin <daniel.nikitin@tptlive.ee>
2024-03-15 01:00:22 +0200

    test dep

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./Cart.css";
 import Button from "../Button/Button";
-import { loadStripe } from "@stripe/stripe-js";  // stripe.com
+// import { loadStripe } from "@stripe/stripe-js";  // stripe.com
 
 
 function Cart({ cartItems, onCheckout }) {
   const totalPrice = cartItems.reduce((a, c) => a + c.price * c.quantity, 0);
 
-  const makePayment = async () => {
-    const stripe = await loadStripe("pk_live_51Ou1aNP9lLnwPkwIXL16K9BAlr0XZ2i9UaubUw7gkwlPh69EAWL2sf2wI2l8XB8gUZrd5K7tbucwaChp3xuH00F300O1WpQHfy");
-  }
+  // const makePayment = async () => {
+  //   const stripe = await loadStripe("pk_live_51Ou1aNP9lLnwPkwIXL16K9BAlr0XZ2i9UaubUw7gkwlPh69EAWL2sf2wI2l8XB8gUZrd5K7tbucwaChp3xuH00F300O1WpQHfy");
+  // }
 
 
   return (
