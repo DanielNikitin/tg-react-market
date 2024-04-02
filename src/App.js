7a849916950cd415c32235eb0b72b1a5a4a2f463
Daniel Nikitin <daniel.nikitin@tptlive.ee>
2024-04-03 02:15:00 +0300

    end

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Main from "./Components/Main/main";
-import Menu from "./Components/Menu/menu";
 import Food from "./Components/Food/Food";
 import PageNotFound from "./Components/404/PageNotFound";
+
 
 function App() {
   return (
     <div className="App">
       <Router>
-        {/* <Link to="/menu">Menu </Link> */}
-        {/* <Link to="/food">Market </Link> */}
         <Routes>
-          <Route path="/" element={<PageNotFound />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/food" element={<Food />} />
+          <Route path="/" element={<Food />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </Router>
